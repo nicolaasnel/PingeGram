951c44162eb595dab213b0bc11859c43e0317722
Nicolaas  Nel <nicolaas.cm.nel@gmail.com>
2017-11-12 00:38:59 +0200

    Correct message chat link

--- app/services/telegram.rb
+++ app/services/telegram.rb
@@ -2,13 +2,13 @@ require 'net/http'
 
 class Telegram
   def send(chat_service, message)
-    url = parse_url(chat_service.chat, message)
+    url = parse_url(chat_service.telegram_chat, message)
     result = Net::HTTP.get(url)
   end
 
   private
 
-    def parse_url(chat_service, message)
+    def parse_url(chat, message)
       URI.parse("https://api.telegram.org/#{chat.bot_token}/sendMessage?text=#{message}&chat_id=#{chat.chat_id}&parse_mode=Markdown")
     end
 
