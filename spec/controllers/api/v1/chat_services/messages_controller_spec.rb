66a835196980fa1cf67cfcb42c20581e00d5b826
Nicolaas  Nel <nicolaas.cm.nel@gmail.com>
2017-11-12 21:50:32 +0200

    Add specs for message sending

--- spec/controllers/api/v1/chat_services/messages_controller_spec.rb
+++ spec/controllers/api/v1/chat_services/messages_controller_spec.rb
@@ -1,19 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::ChatServices::MessagesController, type: :controller do
+  let(:pingdom) { create(:pingdom) }
+  let(:chat)    { create(:telegram_chat, services: [pingdom]) }
+
+  before do
+    request.headers['Authorization'] = "Token token=#{chat.user_id}"
+    request.headers['Accept']        = 'application/json'
+  end
 
   describe "POST #create" do
-    let(:pingdom) { create(:pingdom) }
-    let(:chat) { create(:telegram_chat, services: [pingdom]) }
-
     before do
-      request.headers['Authorization'] = "Token token=#{chat.user_id}"
-      request.headers['Accept']        = 'application/json'
-
       post :create, params: { chat_service_id: chat.chat_services.first.id }
     end
 
     it { expect(response).to have_http_status(:no_content) }
+    it 'should send a message' do
+      expect(Message.count).to eq(1)
+    end
+  end
+
+  describe "GET #show" do
+    before do
+      get :show, params: { chat_service_id: chat.chat_services.first.id }
+    end
+
+    it { expect(response).to have_http_status(:no_content) }
+    it 'should send a message' do
+      expect(Message.count).to eq(1)
+    end
   end
 
 end
