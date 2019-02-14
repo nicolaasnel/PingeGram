4a897b74f00a24f1ece10bdc60d422be8c60a1bf
Nico <nicolaas.cm.nel@gmail.com>
2019-02-14 20:37:58 +0200

    Update ruby

--- spec/factories/telegram_chat.rb
+++ spec/factories/telegram_chat.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :telegram_chat do
-    name { 'Test Telegram Chat' }
+    name      { 'Test Telegram Chat' }
     bot_token { 'test_token' }
-    chat_id { 'chat123' }
+    chat_id   { 'chat123' }
     user
   end
 end
